670793b8d319dfd7d530eb287738a3ce5da0751e
Malik Endsley (R15 Laptop) <malik.s.endsley@gmail.com>
2023-02-09 17:43:43 -0500

    Thresholding improved with blur, debug flag and directory fixed

--- HW1/assignment.py
+++ HW1/assignment.py
@@ -7,8 +7,8 @@ import sys
 
 # constants
 
-LIBRARY_PATH_IN = 'HW1\library\in'
-LIBRARY_PATH_OUT = 'HW1\library\out'
+LIBRARY_PATH_IN = os.path.join(os.getcwd(), 'library\in')
+LIBRARY_PATH_OUT = os.path.join(os.getcwd(), 'library\out')
 
 # flags
 
@@ -19,14 +19,15 @@ def main():
     
     if '--debug' in sys.argv:
         print("Running in debug mode")
+        global DEBUG
         DEBUG = True
     
     # walk through the in folder and run the getHand function on each image
     for root, dirs, files in os.walk(LIBRARY_PATH_IN):
         for file in files:
             if file.endswith('.jpg'):
-                path = os.path.join(root, file)
-                getHand(path)
+                print("Processing file: " + file)
+                getHand(os.path.join(root, file))
     
     
 # this function outputs a dictionary containing all the information about the hand
@@ -35,19 +36,28 @@ def getHand(path) -> dict:
     img = cv.imread(path)
     # convert to HSV
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    # get the red channel
-    red = hsv[:,:,2]
-    # threshold the red channel 
-    ret, thresh = cv.threshold(red, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    # get the value channel
+    v = hsv[:,:,2]
+    # denoise v
+    # threshold the image based on the value channel
+    ret, thresholded_image = cv.threshold(v, 0, 255, cv.THRESH_BINARY |cv.THRESH_OTSU)
+    # simple denoise the image
+    thresholded_image = cv.medianBlur(thresholded_image, 55)
+    # show the intermediate image if the debug flag is set
+    if DEBUG:
+        img2 = cv.resize(thresholded_image, (1000, 1000))
+        cv.imshow('image', img2)
+        cv.waitKey(0)
     # find the contours
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(thresholded_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     # get the largest contour and remove the rest
     contours = sorted(contours, key=cv.contourArea, reverse=True)
     contours = contours[:1]
-    # draw the largest contour over the image and show it
-    cv.drawContours(img, contours, -1, (0,255,0), 3)
-    # resize the image to 1000x1000
-    
+        
+    # draw the largest contour over the image in blue and show it
+    cv.drawContours(img, contours, -1, (255,0,0), 3)
+
+    # show these images if the debug flag is set    
     if DEBUG:
         img2 = cv.resize(img, (1000, 1000))
         cv.imshow('image', img2)
